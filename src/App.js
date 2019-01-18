85447460c0f8bfb1cbb4a8affdab8aa9036f9a2a
Shadman <shadman005@gmail.com>
2019-01-18 21:10:39 +0530

    commit after Buuton js created

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import  ProductList  from "./components/ProductList";
+import  Cart  from "./components/Cart";
+import  Default  from "./components/Default";
+import  Details  from "./components/Details";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList}></Route>
+          <Route path="/details" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route component={Default}></Route>
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Cart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Cart extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Your Cart Is Currently Empty</h3>
+      </div>
+    )
+  }
+}
